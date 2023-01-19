e9aab159c59f2780dddc99f8cf52077374647778
Aman-ABJIMA <aman.singh@abjima.com>
2023-01-19 18:39:39 +0530

    Add project files.

---
+++ Controllers/ClubController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationMVC.Data;
+
+namespace WebApplicationMVC.Controllers
+{
+      
+    public class ClubController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClubController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index(ApplicationDbContext context)//c
+        {
+            var clubs = _context.Clubs.ToList();//m
+            return View();//v
+        }
+    }
+}
---
+++ Data/Enums/RaceCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplicationMVC.Data.Enums
+{
+    public enum RaceCategory
+    {
+        Dravidian,
+        Indo_Aryan,
+        Mongoloid,
+        Aryo_Dravidian,
+        Mongolo_Dravidian
+    }
+}
